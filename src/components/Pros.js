5ef11ec46b2d190ea49e79e9d7c4b308ace7a749
dannyfrelink <danny-frelink@hotmail.com>
2024-02-06 16:07:41 +0100

    feat: set Pros section in component

---
+++ src/components/Pros.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Pros = ({ pro, openPros, setOpenPros }) => {
+	return (
+		<article
+			onClick={() => {
+				if (openPros !== pro.title) {
+					setOpenPros(pro.title);
+				} else {
+					setOpenPros("");
+				}
+			}}
+		>
+			<div>
+				<div>
+					<img src={pro.icon.src} alt={pro.icon.alt} />
+
+					<h3>{pro.title}</h3>
+				</div>
+
+				<h3>{pro.title === openPros ? "-" : "+"}</h3>
+			</div>
+
+			{pro.title === openPros && <p>{pro.description}</p>}
+		</article>
+	);
+};
+
+export default Pros;
