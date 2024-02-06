8bb4db9803c8ad600fa2665f87cc6f411c40050b
dannyfrelink <danny-frelink@hotmail.com>
2024-02-06 12:47:00 +0100

    feat: create header on website and import images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
+import { useState } from "react";
 import "./App.css";
+import logo from "./logo.svg";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import { Link, Element } from "react-scroll";
+import coverImage from "./assets/images/cover.JPG";
 
 function App() {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+	const [navOpen, setNavOpen] = useState(false);
+	const [closeNav, setCloseNav] = useState(false);
+	const navItems = [
+		"Voordelen",
+		"Diensten",
+		"Reviews",
+		"Over mij",
+		"Proces",
+		"Contact",
+	];
+
+	return (
+		<div>
+			<header>
+				<img src={logo} alt="Logo ContentFlow Media" />
+
+				<button onClick={() => setNavOpen(true)}>
+					<MenuRoundedIcon fontSize="inherit" />
+				</button>
+
+				{navOpen && (
+					<nav className={closeNav ? "closeNav" : ""}>
+						{navItems.map((item, index) => (
+							<li key={index}>
+								<Link
+									onClick={() => {
+										setCloseNav(true);
+
+										const timeout = setTimeout(() => {
+											setNavOpen(false);
+											setCloseNav(false);
+										}, 300);
+
+										return () => clearTimeout(timeout);
+									}}
+									to={item.toLowerCase().split(" ").join("-")}
+									smooth={true}
+									offset={-100}
+									duration={500}
+								>
+									{item}
+								</Link>
+							</li>
+						))}
+					</nav>
+				)}
+			</header>
+
+			<main></main>
+
+			<footer></footer>
+		</div>
+	);
 }
 
 export default App;
