ce2d14cb6e6749833e02536cf2f6372c0fc00492
dannyfrelink <danny-frelink@hotmail.com>
2024-02-06 14:56:33 +0100

    update: create pros section for mobile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import logo from "./logo.svg";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import { Link, Element } from "react-scroll";
 import coverImage from "./assets/images/cover.JPG";
+import timeIcon from "./assets/icons/time.png";
+import performanceIcon from "./assets/icons/performance.png";
+import strategyIcon from "./assets/icons/strategy.png";
+import contentIcon from "./assets/icons/content.png";
+import relaxIcon from "./assets/icons/relax.png";
+import growthIcon from "./assets/icons/growth.png";
 
 function App() {
 	const [navOpen, setNavOpen] = useState(false);
@@ -15,6 +21,67 @@ function App() {
 		"Over mij",
 		"Proces",
 		"Contact",
+	];
+
+	const [openPros, setOpenPros] = useState("");
+	const prosItems = [
+		{
+			title: "Tijdsbesparing",
+			description:
+				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+			icon: {
+				src: timeIcon,
+				alt: "Tijdsbesparing Icoon",
+			},
+		},
+		{
+			title: "Optimalisatie online aanwezigheid",
+			description:
+				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+			icon: performanceIcon,
+			icon: {
+				src: performanceIcon,
+				alt: "Optimalisatie Online Aanwezigheid Icoon",
+			},
+		},
+		{
+			title: "Strategie",
+			description:
+				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+			icon: strategyIcon,
+			icon: {
+				src: strategyIcon,
+				alt: "Strategie Icoon",
+			},
+		},
+		{
+			title: "Content",
+			description:
+				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+			icon: {
+				src: contentIcon,
+				alt: "Content Icoon",
+			},
+		},
+		{
+			title: "Gemoedsrust",
+			description:
+				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+			icon: {
+				src: relaxIcon,
+				alt: "Gemoedsrust Icoon",
+			},
+		},
+		{
+			title: "Groei",
+			description:
+				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+			icon: growthIcon,
+			icon: {
+				src: growthIcon,
+				alt: "Groei Icoon",
+			},
+		},
 	];
 
 	return (
@@ -90,6 +157,51 @@ function App() {
 						</button>
 					</article>
 				</section>
+
+				<Element id="pros" name={navItems[0].toLowerCase()}>
+					<h2>Stel je eens voor...</h2>
+
+					<section>
+						{prosItems.map((item, index) => (
+							<article
+								onClick={() => {
+									if (openPros !== item.title) {
+										setOpenPros(item.title);
+									} else {
+										setOpenPros("");
+									}
+								}}
+								key={index}
+							>
+								<div>
+									<div>
+										<img
+											src={item.icon.src}
+											alt={item.icon.alt}
+										/>
+
+										<h3>{item.title}</h3>
+									</div>
+
+									<h3>
+										{item.title === openPros ? "-" : "+"}
+									</h3>
+								</div>
+
+								{item.title === openPros && (
+									<p>{item.description}</p>
+								)}
+							</article>
+						))}
+					</section>
+
+					<button className="button">Ontdek het aanbod</button>
+				</Element>
+
+				<Element
+					id="services"
+					name={navItems[1].toLowerCase()}
+				></Element>
 			</main>
 
 			<footer></footer>
