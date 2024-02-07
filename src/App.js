ca023d1847bfea5fc315d8b70edfbf0a185e5ef3
dannyfrelink <danny-frelink@hotmail.com>
2024-02-07 13:08:33 +0100

    update: transfer variable Arrays to 'variables.js' file

--- src/App.js
+++ src/App.js
@@ -4,134 +4,15 @@ import logo from "./logo.svg";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import { Link, Element } from "react-scroll";
 import coverImage from "./assets/images/cover.JPG";
-import timeIcon from "./assets/icons/time.png";
-import performanceIcon from "./assets/icons/performance.png";
-import strategyIcon from "./assets/icons/strategy.png";
-import contentIcon from "./assets/icons/content.png";
-import relaxIcon from "./assets/icons/relax.png";
-import growthIcon from "./assets/icons/growth.png";
 import Service from "./components/Service";
 import Pros from "./components/Pros";
 import Carousel from "./components/Carousel";
-import { reviewItems } from "./variables";
+import { navItems, prosItems, reviewItems, servicesItems } from "./variables";
 
 function App() {
 	const [navOpen, setNavOpen] = useState(false);
 	const [closeNav, setCloseNav] = useState(false);
-	const navItems = [
-		"Voordelen",
-		"Diensten",
-		"Reviews",
-		"Over mij",
-		"Proces",
-		"Contact",
-	];
-
 	const [openPros, setOpenPros] = useState("");
-	const prosItems = [
-		{
-			title: "Tijdsbesparing",
-			description:
-				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
-			icon: {
-				src: timeIcon,
-				alt: "Tijdsbesparing Icoon",
-			},
-		},
-		{
-			title: "Optimalisatie online aanwezigheid",
-			description:
-				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
-			icon: {
-				src: performanceIcon,
-				alt: "Optimalisatie Online Aanwezigheid Icoon",
-			},
-		},
-		{
-			title: "Strategie",
-			description:
-				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
-			icon: {
-				src: strategyIcon,
-				alt: "Strategie Icoon",
-			},
-		},
-		{
-			title: "Content",
-			description:
-				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
-			icon: {
-				src: contentIcon,
-				alt: "Content Icoon",
-			},
-		},
-		{
-			title: "Gemoedsrust",
-			description:
-				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
-			icon: {
-				src: relaxIcon,
-				alt: "Gemoedsrust Icoon",
-			},
-		},
-		{
-			title: "Groei",
-			description:
-				"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
-			icon: {
-				src: growthIcon,
-				alt: "Groei Icoon",
-			},
-		},
-	];
-
-	const servicesItems = [
-		{
-			number: "01",
-			title: "Social Media Beheer",
-			description:
-				"Social Media Management is the day-to-day curation of your social platforms, where we skillfully bring your unique strategy and aesthetic to life. This way, you can focus on other priorities while we handle your social media.",
-			included: [
-				"Concept",
-				"Script Writing",
-				"Video / Photography",
-				"Editing",
-				"1 Free Revision",
-				"30 Days Paid Advertising",
-				"Google Drive or WeTransfer access to all content",
-			],
-		},
-		{
-			number: "02",
-			title: "Branding",
-			description:
-				"If you’re looking for User Generated Content to share on your social platforms, then this is for you. Whether you need organic or paid ad videos for your social media, we’ve got you covered. We provide both one-off and monthly retainer packages, tailored to your business’s specific requirements.",
-			included: [
-				"Concept",
-				"Script Writing",
-				"Video / Photography",
-				"Editing",
-				"1 Free Revision",
-				"30 Days Paid Advertising",
-				"Google Drive or WeTransfer access to all content",
-			],
-		},
-		{
-			number: "03",
-			title: "Content Creatie",
-			description:
-				"Our Content Creation service is the ideal complement to our Social Media Management packages or can be a standalone solution for those seeking content for one-time projects. It may involve receiving products for us to create content with, or we can come to your location, depending on your unique needs. We’re always ready to explore what we can do to meet your specific content requirements.",
-			included: [
-				"Concept",
-				"Script Writing",
-				"Video / Photography",
-				"Editing",
-				"1 Free Revision",
-				"30 Days Paid Advertising",
-				"Google Drive or WeTransfer access to all content",
-			],
-		},
-	];
 
 	return (
 		<div>
--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,124 @@
+import timeIcon from "./assets/icons/time.png";
+import performanceIcon from "./assets/icons/performance.png";
+import strategyIcon from "./assets/icons/strategy.png";
+import contentIcon from "./assets/icons/content.png";
+import relaxIcon from "./assets/icons/relax.png";
+import growthIcon from "./assets/icons/growth.png";
+
+export const navItems = [
+	"Voordelen",
+	"Diensten",
+	"Reviews",
+	"Over mij",
+	"Proces",
+	"Contact",
+];
+
+export const prosItems = [
+	{
+		title: "Tijdsbesparing",
+		description:
+			"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+		icon: {
+			src: timeIcon,
+			alt: "Tijdsbesparing Icoon",
+		},
+	},
+	{
+		title: "Optimalisatie online aanwezigheid",
+		description:
+			"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+		icon: {
+			src: performanceIcon,
+			alt: "Optimalisatie Online Aanwezigheid Icoon",
+		},
+	},
+	{
+		title: "Strategie",
+		description:
+			"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+		icon: {
+			src: strategyIcon,
+			alt: "Strategie Icoon",
+		},
+	},
+	{
+		title: "Content",
+		description:
+			"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+		icon: {
+			src: contentIcon,
+			alt: "Content Icoon",
+		},
+	},
+	{
+		title: "Gemoedsrust",
+		description:
+			"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+		icon: {
+			src: relaxIcon,
+			alt: "Gemoedsrust Icoon",
+		},
+	},
+	{
+		title: "Groei",
+		description:
+			"Door social media uit te besteden kun je jouw kostbare tijd, aandacht en energie besteden aan je core business.",
+		icon: {
+			src: growthIcon,
+			alt: "Groei Icoon",
+		},
+	},
+];
+
+export const servicesItems = [
+	{
+		number: "01",
+		title: "Social Media Beheer",
+		description:
+			"Social Media Management is the day-to-day curation of your social platforms, where we skillfully bring your unique strategy and aesthetic to life. This way, you can focus on other priorities while we handle your social media.",
+		included: [
+			"Concept",
+			"Script Writing",
+			"Video / Photography",
+			"Editing",
+			"1 Free Revision",
+			"30 Days Paid Advertising",
+			"Google Drive or WeTransfer access to all content",
+		],
+	},
+	{
+		number: "02",
+		title: "Branding",
+		description:
+			"If you’re looking for User Generated Content to share on your social platforms, then this is for you. Whether you need organic or paid ad videos for your social media, we’ve got you covered. We provide both one-off and monthly retainer packages, tailored to your business’s specific requirements.",
+		included: [
+			"Concept",
+			"Script Writing",
+			"Video / Photography",
+			"Editing",
+			"1 Free Revision",
+			"30 Days Paid Advertising",
+			"Google Drive or WeTransfer access to all content",
+		],
+	},
+	{
+		number: "03",
+		title: "Content Creatie",
+		description:
+			"Our Content Creation service is the ideal complement to our Social Media Management packages or can be a standalone solution for those seeking content for one-time projects. It may involve receiving products for us to create content with, or we can come to your location, depending on your unique needs. We’re always ready to explore what we can do to meet your specific content requirements.",
+		included: [
+			"Concept",
+			"Script Writing",
+			"Video / Photography",
+			"Editing",
+			"1 Free Revision",
+			"30 Days Paid Advertising",
+			"Google Drive or WeTransfer access to all content",
+		],
+	},
+];
+
 export const reviewItems = [
 	{
 		review: "It was an absolute pleasure working with Tessa on our UGC project. She captured what we had in mind for the content perfectly, and the results in the video and images were flawless! She has a real talent for showcasing products in their best light and the quality was so high. It ended up being one of our best performing videos on social media with high engagement. We can’t wait to work with her again for future projects!",
